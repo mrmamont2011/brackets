c21b405d72b3442927b42a455e23998a6e5dd25c
mrmamont2011 <60566644+mrmamont2011@users.noreply.github.com>
2020-03-14 22:29:09 +0300

    feat: tasks brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+
+module.exports = function check(str, br) {
+
+let g=0;
+let po;
+
+        
+
+
+function dubl(stroka){
+ 
+    if(stroka=='') return '';
+    
+    let str1='';
+    let flag=0;
+
+    for(let i=0;i<br.length;i++){
+        if(stroka.indexOf(br[i][0]+br[i][1])!=-1) {
+            flag=1; 
+            po=stroka.indexOf(br[i][0]+br[i][1]);
+
+            str1=stroka.slice(0,po) +stroka.slice(po+2) ;                                                
+            break;     
+        }
+
+        
+        
+
+    }
+    g++;
+   
+    
+    if (flag==1) return dubl(str1);
+    
+    if (flag==0) return '*';
+
 }
+
+   
+    if (dubl(str).length==0) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+}
+
